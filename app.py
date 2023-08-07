01fdefca42c10f03007e72278d315e6c2b38f62e
SebastianBenini <seba.benini@gmail.com>
2023-08-07 19:18:52 -0300

    primer commit

---
+++ app.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Text, Optional
+from datetime import datetime
+from uuid import uuid4 as uuid
+
+app = FastAPI()
+
+class Post(BaseModel):
+    id: Optional[str]
+    title: str
+    author: str
+    content: Text
+    created_at: datetime = datetime.now()
+    published_at : Optional[datetime]
+    published: bool = False
+
+posts=[]
+
+@app.get('/')
+def read_root():
+    return{"welcome": "Welcome to my REST API"}
+
+@app.get('/posts')
+def get_posts():
+    return posts
+
+# CREATE
+@app.post('/posts')
+def save_posts(post: Post):
+    post.id = str(uuid())
+    posts.append(post.model_dump())
+    return posts[-1]
+
+# READ
+@app.get('/posts/{post_id}')
+def get_post(post_id:str):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=404, detail="Post Not Found")
+    print(post_id)
+
+# DELETE
+@app.delete('/posts/{post_id}')
+def delete_post(post_id:str):
+    for index,post in enumerate(posts):
+        if post["id"] == post_id:
+            posts.pop(index)
+            return {"message":"Post has been deleted successfully"}
+    raise HTTPException(status_code=404, detail="Post not found")
+   
+# UPDATE      
+@app.put('/posts/{post_id}')
+def update_post(post_id: str, updatePost: Post):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts[index]["title"] = updatePost.title
+            posts[index]["content"] = updatePost.content
+            posts[index]["author"] = updatePost.author
+            return {"message":"Post has been updated successfully"}
+    raise HTTPException(status_code=404, detail="Post not found")
